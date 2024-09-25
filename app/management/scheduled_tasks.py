c8c49d6ef3c3cb03ecd2726411937e300ddb5ede
victorhts <victor.hts.dev@gmail.com.br>
2024-09-24 21:35:10 -0300

    adicionado with current_app.app_context(): para pegar a instancia de banco

--- app/management/scheduled_tasks.py
+++ app/management/scheduled_tasks.py
@@ -1,14 +1,16 @@
 import atexit
+from flask import current_app
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.services.data_log_services import get_data_from_embrapa
 
 
 def run_embrapa_task():
     try:
-        get_data_from_embrapa('producao')
-        get_data_from_embrapa('exportacaoUva')
-        get_data_from_embrapa('exportacaoEspumantes')
-        get_data_from_embrapa('exportacaoVinhoMesa')
+        with current_app.app_context():
+            get_data_from_embrapa('producao')
+            get_data_from_embrapa('exportacaoUva')
+            get_data_from_embrapa('exportacaoEspumantes')
+            get_data_from_embrapa('exportacaoVinhoMesa')
 
     except Exception as e:
         pass
