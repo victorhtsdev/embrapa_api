c6bdc3fe24e2515f49527cf39d48e933c8715004
victorhts <victor.hts.dev@gmail.com>
2024-10-20 17:38:37 -0300

    alteraçoes para o heroku

--- app/config.py
+++ app/config.py
@@ -3,7 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    db_url = os.getenv('DATABASE_URL')
+
+    # Adaptaçao para Heroku
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
